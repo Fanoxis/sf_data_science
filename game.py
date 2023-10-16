e6773b8d68394a323f899cdf92532e613817aff8
Fanoxis <fanoxis@mail.ru>
2023-10-16 15:11:21 +0300

    First Commit

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """Игра угадай число"""
 import numpy as np
 
@@ -16,4 +17,24 @@ while True:
         
     else:
         print(f"Вы угадали число! За {count} попыток")
+=======
+"""Игра угадай число"""
+import numpy as np
+
+number = np.random.randint(1, 101) # Загадываем число
+count = 0
+
+while True:
+    count += 1
+    predict_number = int(input("Угадай число от 1 до 100: "))
+    
+    if predict_number > number:
+        print("Число должно быть меньше")
+        
+    elif predict_number < number:
+        print("Число должно быть больше")
+        
+    else:
+        print(f"Вы угадали число! За {count} попыток")
+>>>>>>> caa4a5f (First Commit)
         break
